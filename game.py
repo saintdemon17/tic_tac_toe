b8ac75f332dddadb97c08d8aca5890bf63344280
saintdemon17 <sement7sev@gmail.com>
2026-04-16 01:07:47 +0300

    Added the "main" function

--- game.py
+++ game.py
@@ -19,11 +19,15 @@ game.display()
 
 
 
-from gameparts  import Board
 
-if __name__ == '__main__':
+from gameparts import Board
+
+def main():
     game = Board()
     game.display()
     game.make_move(1, 1, 'X')
     print('Ход сделан!')
-    game.display() 
+    game.display()
+
+if __name__ == '__main__':
+    main()
