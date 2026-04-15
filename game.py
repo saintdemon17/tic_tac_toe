add92e2aec733b9edd47a46ef1b9460b42c1f3cf
saintdemon17 <sement7sev@gmail.com>
2026-04-16 02:05:05 +0300

    A few more changes have been made

--- game.py
+++ game.py
@@ -22,10 +22,16 @@ game.display()
 
 from gameparts import Board
 
+
+from gameparts.exceptions import FieldIndexError
+
+
 def main():
     game = Board()
     game.display()
     row = int(input('Введите номер строки: '))
+    if row < 0 or row >= game.field_size:
+        raise FieldIndexError
     column = int(input('Введите номер столбца: '))
     game.make_move(row, column, 'X')
     print('Ход сделан!')
