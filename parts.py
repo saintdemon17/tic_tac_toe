a5482d3a8722e30bfca4a953bf6d4f89b6e32e0a
saintdemon17 <sement7sev@gmail.com>
2026-04-16 02:52:20 +0300

    Several functions have been added

--- parts.py
+++ parts.py
@@ -2,7 +2,7 @@ class Board:
     """Класс, который описывает игровое поле."""
 
     field_size = 3
-    
+
     def __init__(self):
         self.board = [[' ' for _ in range(3)] for _ in range(3)]
 
@@ -14,6 +14,26 @@ class Board:
             print('|'.join(row))
             print('-' * 5)
 
+    def is_board_full(self):
+        for i in range(self.field_size):
+            for j in range(self.field_size):
+                if self.board[i][j] == ' ':
+                    return False
+        return True
+    def check_win(self, player):
+        for i in range(3):
+            if (all([self.board[i][j] == player for j in range(3)]) or
+                    all([self.board[j][i] == player for j in range(3)])):
+                return True
+        if (
+            self.board[0][0] == self.board[1][1] == self.board[2][2] == player
+            or
+            self.board[0][2] == self.board[1][1] == self.board[2][0] == player
+        ):
+            return True
+
+        return False
+    
     def __str__(self):
         return (
             'Объект игрового поля размером '
