389d259d3f689dfc8206f3a8334d4113cb3a15c0
saintdemon17 <sement7sev@gmail.com>
2026-04-16 01:44:28 +0300

    Added docstring

--- parts.py
+++ parts.py
@@ -1,4 +1,8 @@
 class Board:
+    """Класс, который описывает игровое поле."""
+
+    field_size = 3
+    
     def __init__(self):
         self.board = [[' ' for _ in range(3)] for _ in range(3)]
 
