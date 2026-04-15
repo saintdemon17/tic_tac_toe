ec73dec659ffb718b454e5ce89744effe45acb6e
saintdemon17 <sement7sev@gmail.com>
2026-04-16 00:58:49 +0300

    Added the if __name__ == '__main__' construct

--- game.py
+++ game.py
@@ -16,3 +16,14 @@ game.display()
 game.make_move(1, 1, 'X')
 print('Ход сделан!')
 game.display()
+
+
+
+from gameparts  import Board
+
+if __name__ == '__main__':
+    game = Board()
+    game.display()
+    game.make_move(1, 1, 'X')
+    print('Ход сделан!')
+    game.display() 
