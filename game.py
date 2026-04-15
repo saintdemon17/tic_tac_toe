1334ff61391369f79b1219c1d717ef32ba07416c
saintdemon17 <sement7sev@gmail.com>
2026-04-16 02:22:27 +0300

    Added several treatments

--- game.py
+++ game.py
@@ -21,20 +21,35 @@ game.display()
 
 
 from gameparts import Board
-
-
 from gameparts.exceptions import FieldIndexError
 
 
 def main():
     game = Board()
     game.display()
-    row = int(input('Введите номер строки: '))
-    if row < 0 or row >= game.field_size:
-        raise FieldIndexError
-    column = int(input('Введите номер столбца: '))
-    if column < 0 or column >= game.field_size:
-        raise FieldIndexError
+    while True:
+        try:
+            row = int(input('Введите номер строки: '))
+            if row < 0 or row >= game.field_size:
+                raise FieldIndexError
+            column = int(input('Введите номер столбца: '))
+            if column < 0 or column >= game.field_size:
+                raise FieldIndexError
+        except FieldIndexError:
+            print(
+                'Значение должно быть неотрицательным и меньше '
+                f'{game.field_size}.'
+            )
+            print('Пожалуйста, введите значения для строки и столбца заново.')
+            continue
+        except ValueError:
+            print('Буквы вводить нельзя. Только числа.')
+            print('Пожалуйста, введите значения для строки и столбца заново.')
+            continue
+        except Exception as e:
+            print(f'Возникла ошибка: {e}')
+        else:
+            break
     game.make_move(row, column, 'X')
     print('Ход сделан!')
     game.display()
