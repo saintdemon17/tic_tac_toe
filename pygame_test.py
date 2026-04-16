a7dd3555e6ff99295ad65eacadc8231010113ada
saintdemon17 <sement7sev@gmail.com>
2026-04-16 03:18:56 +0300

    New changes

--- pygame_test.py
+++ pygame_test.py
@@ -13,5 +13,7 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-
+    pygame.draw.line(screen, (255, 0, 0), (100, 100), (700, 500), 5)
+    pygame.draw.rect(screen, (0, 128, 0), pygame.Rect(300, 200, 200, 200))
+    pygame.display.update()
 pygame.quit()
