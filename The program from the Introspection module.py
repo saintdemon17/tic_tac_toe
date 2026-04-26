44863227aa66abf88acf714a0b814dc067c93a61
saintdemon17 <sement7sev@gmail.com>
2026-04-26 17:19:15 +0300

    Added print(isfunction(game.display))

--- The program from the Introspection module.py
+++ The program from the Introspection module.py
@@ -1,9 +1,12 @@
-# Из модуля inspect импортировать функцию getsource.
-from inspect import getsource
+# Из модуля inspect импортировать функцию isfunction.
+from inspect import isfunction
 
 from gameparts import Board
 
 game = Board()
 
-# Функция getsource() в работе.
-print(getsource(Board))
+# display() - это функция?
+print(isfunction(game.display))
+
+# Выведется:
+# False 
