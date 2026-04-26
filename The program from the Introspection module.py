2197022c1d882b194b6a5fa7b93f82289756e5d8
saintdemon17 <sement7sev@gmail.com>
2026-04-26 17:17:12 +0300

    Added print(getsource(Board))

--- The program from the Introspection module.py
+++ The program from the Introspection module.py
@@ -1,17 +1,9 @@
+# Из модуля inspect импортировать функцию getsource.
+from inspect import getsource
+
 from gameparts import Board
 
 game = Board()
 
-print(game.__class__.__dict__)
-
-# Выведется:
-# {'__module__': 'gameparts.parts', '__init__': <function Board.__init__ at 0x104f95f70>, 'make_move': <function Board.make_move at 0x104fa3040>, 'display': <function Board.display at 0x104fa30d0>, '__dict__': <attribute '__dict__' of 'Board' objects>, '__weakref__': <attribute '__weakref__' of 'Board' objects>, '__doc__': None} 
-
-print(type(game) is Board)
-print(type(game) == Board)
-print(type(game) == str)
-
-# Выведется: 
-# True
-# True
-# False
+# Функция getsource() в работе.
+print(getsource(Board))
