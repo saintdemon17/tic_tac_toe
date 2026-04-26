c2165dee9b5af1a14c14f5b5a21133a9e4efc7c8
saintdemon17 <sement7sev@gmail.com>
2026-04-26 16:43:31 +0300

    The Introspection module has been expanded

--- The program from the Introspection module.py
+++ The program from the Introspection module.py
@@ -5,3 +5,12 @@ print(type(game))
 
 # Выведется:
 # <class 'gameparts.parts.Board'>
+
+print(type(game) is Board)
+print(type(game) == Board)
+print(type(game) == str)
+
+# Выведется: 
+# True
+# True
+# False
