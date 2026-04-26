aa35d6f1d5ecdafe07af5390d81d8f456e46fb3e
saintdemon17 <sement7sev@gmail.com>
2026-04-26 17:16:17 +0300

    Added print(game.__class__.__dict__)

--- The program from the Introspection module.py
+++ The program from the Introspection module.py
@@ -2,10 +2,10 @@ from gameparts import Board
 
 game = Board()
 
-print(hasattr(game, '__str__'))
+print(game.__class__.__dict__)
 
 # Выведется:
-# True
+# {'__module__': 'gameparts.parts', '__init__': <function Board.__init__ at 0x104f95f70>, 'make_move': <function Board.make_move at 0x104fa3040>, 'display': <function Board.display at 0x104fa30d0>, '__dict__': <attribute '__dict__' of 'Board' objects>, '__weakref__': <attribute '__weakref__' of 'Board' objects>, '__doc__': None} 
 
 print(type(game) is Board)
 print(type(game) == Board)
